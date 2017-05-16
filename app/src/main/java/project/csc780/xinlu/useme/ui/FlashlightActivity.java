8a9b71d65b424f16282f95a0997460cfe0d88a76
Chrissy <xchen4@mail.sfsu.edu>
2017-05-15 17:53:51 -0700

    flashlight ui and function done

--- app/src/main/java/project/csc780/xinlu/useme/ui/FlashlightActivity.java
+++ app/src/main/java/project/csc780/xinlu/useme/ui/FlashlightActivity.java
@@ -1,7 +1,22 @@
 package project.csc780.xinlu.useme.ui;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import project.csc780.xinlu.useme.R;
 
@@ -11,8 +26,121 @@ import project.csc780.xinlu.useme.R;
 
 public class FlashlightActivity extends AppCompatActivity {
 
+    private static final String TAG = FlashlightActivity.class.getSimpleName();
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ImageView mFlashlightImageView;
+    private MediaPlayer mMediaPlayer;
+    private Boolean isTorchOn;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pedometer);
+        setContentView(R.layout.activity_flashlight);
+        mFlashlightImageView = (ImageView) findViewById(R.id.flashlightImageView);
+        isTorchOn = false;
+        //Check if device contains flashlight
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!isFlashAvailable) {
+            AlertDialog alert = new AlertDialog.Builder(FlashlightActivity.this).create();
+            alert.setTitle(getString(R.string.app_name));
+            alert.setMessage(getString(R.string.flashlight_error));
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.confirm), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        mFlashlightImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isTorchOn) {
+                        turnOffLight();
+                        Log.d(TAG, "turn off light");
+                        isTorchOn = false;
+                    } else {
+                        turnOnLight();
+                        Log.d(TAG, "turn on light");
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void turnOnLight() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+                //mFlashlightImageView.setImageResource(R.drawable.on);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void turnOffLight() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+                //mFlashlightImageView.setImageResource(R.drawable.off);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isTorchOn) {
+            turnOffLight();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isTorchOn) {
+            turnOffLight();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isTorchOn) {
+            turnOnLight();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.homeAction:
+                startActivity(new Intent(FlashlightActivity.this, MainActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
